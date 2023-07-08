7ebd0e357fd4edb0e30b35bb7f0ec01f5aa3fd0e
tjfehdgns1 <tjfehdgns@gmail.com>
2023-07-08 23:59:29 +0900

    add unfinnished anim

--- ui/start_menu.gd
+++ ui/start_menu.gd
@@ -7,11 +7,12 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	start_button.grab_focus()
+	start_button.grab_click_focus()
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if start_button.button_up:
-		get_tree().change_scene_to_file("res://main.tscn")
+	pass
 		
